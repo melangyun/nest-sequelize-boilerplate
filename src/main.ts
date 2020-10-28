c0deb0fa18151d091550c3d0bcf2de39ef1389dc
melangyun <melangyun@gmail.com>
2020-10-28 16:02:47 +0900

    add sentry

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { init as SentryInit } from '@sentry/node';
 import { AppModule } from './modules/app.module';
 import { LoggingInterceptor } from './logging.interceptor';
-
 // import { ValidationPipe } from '@nestjs/common';
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const nodeEnv = process.env.NODE_ENV;
   // app.useGlobalPipes(new ValidationPipe());
+  SentryInit({
+    dsn: nodeEnv ? process.env.SENTRY_DNS : '',
+  });
   app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(3000);
 
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { RavenModule, RavenInterceptor } from 'nest-raven';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [SharedModule, UsersModule],
+  imports: [SharedModule, UsersModule, RavenModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useValue: new RavenInterceptor(),
+    },
+  ],
 })
 export class AppModule {}
